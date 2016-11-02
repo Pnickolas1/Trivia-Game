67cc377ef10927b36ea17b89647e4f9fb0563d80
Peter Tountas <petertountas1@gmail.com>
2016-11-02 17:16:38 -0500

    more work

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,37 +2,43 @@ $(document).ready();
 
 alert("test to dom");
 
+
+document.addEventListener("click", function(){
+    document.getElementById("question").innerHTML = "Hello World";
+});
+
+
+
 var questionOne = [
 {
 	options: ["Ty Cobb,40", "Pete Rose,44", "Willie Keeler,45", "Rogers Hornsby,49"],
 	q: "After Joe DiMaggio with 56 consecutive games with a hit, who is 2nd and how many games?", 
-	a: 2
-
+	a: 2,
 }
 ]
 
-
 var questionTwo = [
 {
-	options: ["Gaylord Perry","Roger Clemens","Babe Ruth","Andy Pettite"]
-	q:"Name the first pitcher to win the Cy Young Award in both leagues."
-	a: 0
+	options: ["Gaylord Perry","Roger Clemens","Babe Ruth","Andy Pettite"],
+	q:"Name the first pitcher to win the Cy Young Award in both leagues.",
+	a: 0,
 }
  ]
 
-
  var questioThree =[
  {
- 	options: ["Mickey Mantle","Jack Robinson","Lou Gehrig","Babe Ruth"]
- 	q: "Whose number was the first ever retired by an MLB team?"
- 	a: 2
+ 	options: ["Mickey Mantle","Jack Robinson","Lou Gehrig","Babe Ruth"],
+ 	q: "Whose number was the first ever retired by an MLB team?",
+ 	a: 2,
  }
  ]
 
  var questionFour =[
 {
-	options:["Cincinnati Reds","Boston Red Sox","New York Yankees","Detroit Tigers"]
-	q:"Who did the Chicago Cubs play in the 1908  World Series?"
-	a: 3
+	options:["Cincinnati Reds","Boston Red Sox","New York Yankees","Detroit Tigers"],
+	q:"Who did the Chicago Cubs play in the 1908  World Series?",
+	a: 3,
 }
  ]
+
+
