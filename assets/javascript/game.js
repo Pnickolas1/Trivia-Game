723da8a36904b4d5befd85597a15e472faa61acf
Peter Tountas <petertountas1@gmail.com>
2016-11-01 23:47:11 -0500

    more adds

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,38 @@
 $(document).ready();
 
 alert("test to dom");
+
+var questionOne = [
+{
+	options: ["Ty Cobb,40", "Pete Rose,44", "Willie Keeler,45", "Rogers Hornsby,49"],
+	q: "After Joe DiMaggio with 56 consecutive games with a hit, who is 2nd and how many games?", 
+	a: 2
+
+}
+]
+
+
+var questionTwo = [
+{
+	options: ["Gaylord Perry","Roger Clemens","Babe Ruth","Andy Pettite"]
+	q:"Name the first pitcher to win the Cy Young Award in both leagues."
+	a: 0
+}
+ ]
+
+
+ var questioThree =[
+ {
+ 	options: ["Mickey Mantle","Jack Robinson","Lou Gehrig","Babe Ruth"]
+ 	q: "Whose number was the first ever retired by an MLB team?"
+ 	a: 2
+ }
+ ]
+
+ var questionFour =[
+{
+	options:["Cincinnati Reds","Boston Red Sox","New York Yankees","Detroit Tigers"]
+	q:"Who did the Chicago Cubs play in the 1908  World Series?"
+	a: 3
+}
+ ]
